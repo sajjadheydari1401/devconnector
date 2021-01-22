7fc940145569f55fbfac1329a17e325e9a5b4ec3
sajjad heydari <sajjadhaidari1994@gmail.com>
2021-01-22 11:31:03 +0330

    Auth & Validation done

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI:
-    'mongodb+srv://sajjad:sajjad@cluster0.e4gu7.mongodb.net/devconnector?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://sajjad:sajjad@cluster0.e4gu7.mongodb.net/devconnector?retryWrites=true&w=majority',
+  secretOrKey: 'secret'
 };
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -7,24 +9,30 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
-//DB Config
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Config
 const db = require('./config/keys').mongoURI;
 
-//Connect to MongoDB
+// Connect to MongoDB
 mongoose
-    .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => console.log('MongoDB Connected!'))
-    .catch(err => console.log(err));
+  .connect(db,{useNewUrlParser:true, useUnifiedTopology: true } )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello!'));
+// Passport middleware
+app.use(passport.initialize());
 
-//Use routes
+// Passport Config
+require('./config/passport')(passport);
+
+// Use Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-
+app.listen(port, () => console.log(`Server running on port ${port}`));
